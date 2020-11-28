e428c54109c84d64469c75c970cb5be8177b5569
bhattigurjot <bhattigurjot@gmail.com>
2020-11-28 00:01:06 -0600

    Started reading opcodes

--- src/chippy8.js
+++ src/chippy8.js
@@ -2,7 +2,7 @@ import IO from "./io";
 
 class Chippy8 {
     constructor(canvas) {
-        this.renderer = new IO(canvas, 64, 32, 10);
+        this.io = new IO(canvas, 64, 32, 10);
         
         this.memory = new Uint8Array(4096);
         this.pc = 0;
@@ -39,7 +39,55 @@ class Chippy8 {
     }
 
     emulatorLoop = () => {
-        this.renderer.draw();
+        let oc = this.memory[this.pc] << 8 | this.memory[this.pc + 1];
+        this.decode(oc);
+
+        this.io.draw();
+    }
+
+    decode = (oc) => {
+        this.pc += 2;
+    
+        // X: The second nibble. Used to look up one of the 16 registers (VX) from V0 through VF.
+        // Y: The third nibble. Also used to look up one of the 16 registers (VY) from V0 through VF.
+        // N: The fourth nibble. A 4-bit number.
+        // NN: The second byte (third and fourth nibbles). An 8-bit immediate number.
+        // NNN: The second, third and fourth nibbles. A 12-bit immediate memory address.
+
+        // Implement these two display IBM logo
+        // 00E0 (clear screen)
+        // 1NNN (jump)
+        // 6XNN (set register VX)
+        // 7XNN (add value to register VX)
+        // ANNN (set index register I)
+        // DXYN (display/draw)
+
+        // let x = oc;
+        // let y = oc;
+
+        switch (oc) {
+            case 0x00E0: // CLS
+                this.io.clearScreen();
+                break;
+            case 0x1000: // JP addr
+                this.pc = oc & 0xFFF;
+                break;
+            case 0x6000: // SET
+                // this.v[x] = oc & 0xFF; 
+                break;
+            case 0x7000: // ADD
+                // this.v[x] += oc & 0xFF; 
+                break;
+            case 0xA000:
+                // this.v[x] = oc & 0xFFF;
+                break;
+            case 0xD000: // DRAW
+                
+                break;
+            default:
+                console.log("Wrong opcode: " + oc);
+                break;
+        }
     }
 
 }
